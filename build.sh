6e256cb08995af454598cdf41c452e6653b98b44
YonBer96 <ycorber487@gmail.com>
2026-03-05 16:32:32 +0100

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
+<<<<<<< HEAD
 #!/usr/bin/env bash
 set -o errexit
 
 pip install -r requirements.txt
 python manage.py migrate
+=======
+#!/usr/bin/env bash
+set -o errexit
+
+pip install -r requirements.txt
+python manage.py migrate
+>>>>>>> 230ca905f346a8beb0364862f23e59d304cfc50e
 python manage.py collectstatic --noinput
